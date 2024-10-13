a820805950d34f7fac8ea9e7ee64c9b0b3784bfa
T_Himanshu_123 <himanshusharma2002.2000@gmail.com>
2024-10-13 18:56:36 +0530

    Template added

---
+++ 09-InBuilt-Object/Array.js
@@ -0,0 +1,8 @@
+let arr = [1, 2, 3, 4, 5, 6];
+console.log(arr);
+
+
+// for each loop
+for(let x of arr){
+    console.log(x);
+}
---
+++ 09-InBuilt-Object/template.js
@@ -0,0 +1,15 @@
+let name = "Himanshu Sharma";
+
+// If you have to write " inside string or print statement -->
+// use Escape character.. \" --> for quotations, \n --> new line
+
+
+// BUT with Back tick.. we can do so without using escape character
+
+let message = 
+`
+    My Name 
+    is "${name}"
+`;
+
+console.log(message);
