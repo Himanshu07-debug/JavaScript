f4e49f5e5cb4e42abc7c65cd4a9dffc87edf0428
T_Himanshu_123 <himanshusharma2002.2000@gmail.com>
2024-10-19 00:57:28 +0530

    Array empty-concat-objSearch Added

--- 09-InBuilt-Object/Array.js
+++ 09-InBuilt-Object/Array.js
@@ -35,6 +35,35 @@ console.log(arr.includes(5)); // true
 console.log(arr.includes(50)); // false
 
 
+// If array of Objects THEN ----------------------------------------->>>
+
+let objArr = [
+    {
+        No : 1,
+        Name : 'Himanshu'
+    }
+    ,
+    {
+        No : 2,
+        Name : 'Prish'
+    }
+]
+
+console.log(objArr.includes({No:1, Name:"Himanshu"}));       // false (but it is present)
+
+// includes , indexOf do not work in array of objects.. here find function with callback function is used
+
+let obj = objArr.find(function(obj){
+    return obj.Name === "Himanshu";
+})
+
+
+// Optimized way of writing above thing
+
+let ob = objArr.find((obj) => obj.Name === "Himanshu");
+
+
+
 // Deletion ------------------------------>>
 
 // at beginning
@@ -48,3 +77,37 @@ console.log(arr);
 // Remove at Middle 
 arr.splice(1, 1);
 console.log(arr);
+
+
+
+// Emptying Array ------------------------------>>>
+
+// 1st way
+let ar = [1,2,5,4,7];
+console.log(ar);
+
+let arr1 = ar;   // Addresses are passed
+
+ar = []
+console.log(ar); // Empty
+console.log(arr1);  // Not empty
+
+// Becz you had just point the ar to an new empty array, but arr1 is still pointing to [1,2,5,4,7] array object
+
+
+// 2nd way : BEST
+let arr2 = arr1;
+arr1.length = 0;
+console.log(arr1);
+console.log(arr2);
+
+
+// COMBINING TWO ARRAY ------------------------------->>>
+
+let first = [1, 2, 3, 4];
+let second = [4, 3, 2, 1];
+
+console.log(first, second);
+
+let combine = first.concat(second);
+console.log(combine);
